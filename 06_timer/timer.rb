18322bfc274e9437951f654927afdecea9603710
Chi <cychoi@zoho.com>
2017-01-08 13:57:17 -0700

    Update timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,26 @@
 class Timer
   #write your code here
+  def initialize
+    @seconds = 0
+  end
+  def seconds= seconds
+    @seconds = seconds
+  end
+  def seconds
+    return @seconds
+  end
+  def time_string
+    minutes = 0
+    seconds = @seconds
+    if seconds > 59
+      minutes = seconds / 60
+      seconds = seconds % 60
+    end
+    hours = 0
+    if minutes > 59
+      hours = minutes / 60
+      minutes = minutes % 60
+    end
+    time_string = "%02d:%02d:%02d" % [hours, minutes, seconds]
+  end
 end
